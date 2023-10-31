573c648481b92336a7281431717513cdf3f0480b
HieuNTHE130901 <hieunthe130901@gmail.com>
2023-10-31 21:40:28 +0700

    ok

---
+++ GroceryApplication/app/src/main/java/com/example/groceryapplication/ui/cart/CartFragment.java
@@ -0,0 +1,25 @@
+package com.example.groceryapplication.ui.cart;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.groceryapplication.R;
+
+public class CartFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_cart, container, false);
+        return root;
+    }
+
+}
--- GroceryApplication/app/src/main/java/com/example/groceryapplication/ui/home/HomeFragment.java
+++ GroceryApplication/app/src/main/java/com/example/groceryapplication/ui/home/HomeFragment.java
@@ -1,38 +1,94 @@
 package com.example.groceryapplication.ui.home;
 
-import androidx.lifecycle.ViewModelProvider;
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.groceryapplication.databinding.FragmentHomeBinding;
-import com.example.groceryapplication.ui.home.HomeViewModel;
+import com.example.groceryapplication.R;
+import com.example.groceryapplication.adapter.ItemAdapter;
+import com.example.groceryapplication.models.Item;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    List<Item> fruitList;
+    List<Item> vegetableList;
+    RecyclerView fruitRecycle;
+    RecyclerView vegetableRecycle;
+
+    ItemAdapter itemAdapter1;
+    ItemAdapter itemAdapter2;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        vegetableRecycle = root.findViewById(R.id.rec_vegetable);
+        vegetableRecycle.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        vegetableList = new ArrayList<>();
+        itemAdapter1 = new ItemAdapter(getActivity(), vegetableList);
+        vegetableRecycle.setAdapter(itemAdapter1);
+        db.collection("vegetable")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Item item = document.toObject(Item.class);
+                                vegetableList.add(item);
+                                itemAdapter1.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(),"Error "+ task.getException(), Toast.LENGTH_SHORT);
+                        }
+                    }
+                });
+
+        fruitRecycle = root.findViewById(R.id.rec_fruit);
+        fruitRecycle.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        fruitList = new ArrayList<>();
+        itemAdapter2 = new ItemAdapter(getActivity(), fruitList);
+        fruitRecycle.setAdapter(itemAdapter2);
+        db.collection("fruit")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Item item = document.toObject(Item.class);
+                                fruitList.add(item);
+                                itemAdapter2.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(),"Error "+ task.getException(), Toast.LENGTH_SHORT);
+                        }
+                    }
+                });
+
+
+
+
         return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
 
 }
