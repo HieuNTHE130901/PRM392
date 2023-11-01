b7f2f74c9a5ff0587d9809899e6d08951c8ee29b
HieuNTHE130901 <hieunthe130901@gmail.com>
2023-11-02 00:03:47 +0700

    ok

--- GroceryApplication/app/src/main/java/com/example/groceryapplication/activities/PaymentActivity.java
+++ GroceryApplication/app/src/main/java/com/example/groceryapplication/activities/PaymentActivity.java
@@ -2,10 +2,16 @@ package com.example.groceryapplication.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.groceryapplication.R;
+
+import java.security.MessageDigest;
 
 public class PaymentActivity extends AppCompatActivity {
 
@@ -16,11 +22,24 @@ public class PaymentActivity extends AppCompatActivity {
 
         double totalAmount = getIntent().getDoubleExtra("totalAmount", 0.0);
 
+        String totalAmountString = String.valueOf(totalAmount);
+
         // Find the TextView in the layout
-        TextView totalAmountTextView = findViewById(R.id.totalAmountTextView);
+        TextView totalAmountTextView = findViewById(R.id.total_amt);
 
         // Set the totalAmount to the TextView
-        totalAmountTextView.setText("Total Amount: " + totalAmount);
+        totalAmountTextView.setText(totalAmountString);
+
+        Button buy = findViewById(R.id.pay_btn);
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
 
     }
 }
